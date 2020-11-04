1bdf7c9d9ba42465439d60f9eefaa50f868f18ee
mgjeffries <gib.jeffries@gmail.com>
2020-11-04 10:15:29 -0600

    add maker to game model

--- models/game.py
+++ models/game.py
@@ -4,6 +4,7 @@ from django.db import models
 class Game(models.Model):
     """database game model"""
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    maker = models.CharField(max_length=75)
     number_of_players = models.IntegerField()
     skill_level = models.IntegerField()
     title = models.CharField(max_length=75)
