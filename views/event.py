ee503076fdb11858509ce2e853f6bdbb9835cb64
mgjeffries <gib.jeffries@gmail.com>
2020-11-06 10:19:57 -0600

    add joined property to the event view

--- views/event.py
+++ views/event.py
@@ -95,12 +95,24 @@ class Events(ViewSet):
         Returns:
             Response -- JSON serialized list of events
         """
+        # Get the current authenticated user
+        gamer = Gamer.objects.get(user=request.auth.user)
         events = Event.objects.all()
+
+        # Set the `joined` property on every event
+        for event in events:
+            event.joined = None
+
+            try:
+                EventGamer.objects.get(event=event, gamer=gamer)
+                event.joined = True
+            except EventGamer.DoesNotExist:
+                event.joined = False
 
         # Support filtering events by game
         game = self.request.query_params.get('gameId', None)
         if game is not None:
-            events = events.filter(game__id=game)
+            events = events.filter(game__id=type)
 
         serializer = EventSerializer(
             events, many=True, context={'request': request})
@@ -202,6 +214,6 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
             lookup_field='id'
         )
         fields = ('id', 'url', 'game', 'gamer',
-                  'description', 'day', 'time')
+                  'description', 'day', 'time', 'joined')
 
                   
